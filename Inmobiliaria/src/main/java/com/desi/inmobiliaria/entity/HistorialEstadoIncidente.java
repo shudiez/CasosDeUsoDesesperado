5a758f2fb1fd5b541f77d70de78c1dd00e37e6fe
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2026-06-29 10:01:24 -0300

    Add HistorialEstadoIncidente entity class

---
+++ Inmobiliaria/src/main/java/com/desi/inmobiliaria/entity/HistorialEstadoIncidente.java
@@ -0,0 +1,66 @@
+package com.desi.inmobiliaria.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class HistorialEstadoIncidente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	private EstadoIncidente estado;
+
+	private LocalDateTime fechaHora;
+
+	@ManyToOne(optional = false)
+	private Incidente incidente;
+
+	public HistorialEstadoIncidente() {
+	}
+
+	public HistorialEstadoIncidente(Incidente incidente, EstadoIncidente estado) {
+		this.incidente = incidente;
+		this.estado = estado;
+		this.fechaHora = LocalDateTime.now();
+	}
+
+	// Getters y Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public EstadoIncidente getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoIncidente estado) {
+		this.estado = estado;
+	}
+
+	public LocalDateTime getFechaHora() {
+		return fechaHora;
+	}
+
+	public void setFechaHora(LocalDateTime fechaHora) {
+		this.fechaHora = fechaHora;
+	}
+
+	public Incidente getIncidente() {
+		return incidente;
+	}
+
+	public void setIncidente(Incidente incidente) {
+		this.incidente = incidente;
+	}
+}
