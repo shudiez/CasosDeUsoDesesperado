2605963463d75452bffe6f8851fe50fdb281b694
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2026-06-29 10:04:22 -0300

    Fix formatting and add newline at end of ContratoService.java

--- Inmobiliaria/src/main/java/com/desi/inmobiliaria/service/ContratoService.java
+++ Inmobiliaria/src/main/java/com/desi/inmobiliaria/service/ContratoService.java
@@ -1,5 +1,6 @@
 package com.desi.inmobiliaria.service;
 
+
 import java.time.LocalDate;
 import java.util.List;
 
@@ -7,17 +8,17 @@ import com.desi.inmobiliaria.entity.Contrato;
 import com.desi.inmobiliaria.entity.EstadoContrato;
 
 
-
 public interface ContratoService {
 
-	List<Contrato> listarConFiltros(String propiedad, String inquilino, EstadoContrato estado, LocalDate fechaInicio);
-
-	void guardar(Contrato contrato);
-
-	Contrato buscarPorId(Long id);
-
-	void eliminar(Long id);
-
-	void cambiarEstado(Long contratoId, EstadoContrato nuevoEstado);
-
+	
+	List<Contrato> listarConFiltros(String propiedad, String inquilino, EstadoContrato estado,LocalDate fechaInicio);
+    
+    void guardar(Contrato contrato);
+    
+    Contrato buscarPorId(Long id);
+    
+    void eliminar(Long id);
+    
+    void cambiarEstado(Long contratoId, EstadoContrato nuevoEstado);
+	
 }
