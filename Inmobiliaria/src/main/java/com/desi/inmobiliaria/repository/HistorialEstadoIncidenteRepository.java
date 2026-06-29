89725cc92d9d6482bf02af43c6074006e2cca1ac
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2026-06-29 10:03:20 -0300

    Create HistorialEstadoIncidenteRepository interface

---
+++ Inmobiliaria/src/main/java/com/desi/inmobiliaria/repository/HistorialEstadoIncidenteRepository.java
@@ -0,0 +1,10 @@
+package com.desi.inmobiliaria.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.desi.inmobiliaria.entity.HistorialEstadoIncidente;
+
+public interface HistorialEstadoIncidenteRepository
+		extends JpaRepository<HistorialEstadoIncidente, Long> {
+
+}
