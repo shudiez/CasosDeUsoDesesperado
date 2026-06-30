11f48012053688b4d4826d1c973f4fe777d1104c
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2026-06-30 19:31:21 -0300

    Enhance comments in HistorialEstadoIncidenteRepository
    Added comments explaining the repository functionality.

--- Inmobiliaria/src/main/java/com/desi/inmobiliaria/repository/HistorialEstadoIncidenteRepository.java
+++ Inmobiliaria/src/main/java/com/desi/inmobiliaria/repository/HistorialEstadoIncidenteRepository.java
@@ -3,5 +3,10 @@ package com.desi.inmobiliaria.repository;
 import com.desi.inmobiliaria.entity.HistorialEstadoIncidente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Repository que se encarga de acceder a la tabla del historial de estados de los incidentes
 public interface HistorialEstadoIncidenteRepository extends JpaRepository<HistorialEstadoIncidente, Long> {
+
+   // No hace falta crear métodos porque JpaRepository ya trae
+   // guardar, buscar, listar y eliminar.
+
 }
