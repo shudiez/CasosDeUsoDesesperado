934936f2641c1430098d52daebe472a49e89beff
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2026-06-30 19:36:34 -0300

    Add method comments to PersonaService interface

--- Inmobiliaria/src/main/java/com/desi/inmobiliaria/service/PersonaService.java
+++ Inmobiliaria/src/main/java/com/desi/inmobiliaria/service/PersonaService.java
@@ -4,11 +4,16 @@ import com.desi.inmobiliaria.entity.Persona;
 import java.util.List;
 
 public interface PersonaService {
+
+   // Devuelve todas las personas
    List<Persona> listarTodas();
 
+   // Busca una persona por su id
    Persona buscarPorId(Long id);
 
+   // Guarda una persona nueva o editada
    Persona guardar(Persona persona);
 
+   // Elimina una persona
    void eliminar(Long id);
 }
