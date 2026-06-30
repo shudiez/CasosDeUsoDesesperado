39bf73ab0cd2879514b5616e11b5cd106b938df1
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2026-06-30 19:31:46 -0300

    Update HistorialEstadoPropiedadRepository.java

--- Inmobiliaria/src/main/java/com/desi/inmobiliaria/repository/HistorialEstadoPropiedadRepository.java
+++ Inmobiliaria/src/main/java/com/desi/inmobiliaria/repository/HistorialEstadoPropiedadRepository.java
@@ -3,5 +3,10 @@ package com.desi.inmobiliaria.repository;
 import com.desi.inmobiliaria.entity.HistorialEstadoPropiedad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Repository que se encarga de acceder al historial de estados de las propiedades
 public interface HistorialEstadoPropiedadRepository extends JpaRepository<HistorialEstadoPropiedad, Long> {
+
+   // No agregué métodos porque JpaRepository ya trae
+   // los básicos para guardar, buscar, listar y eliminar.
+
 }
