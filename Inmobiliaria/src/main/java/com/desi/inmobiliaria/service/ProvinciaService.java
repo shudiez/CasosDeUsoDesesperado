ff752353a7a3424708f3f2bb4de9d51dd21c3734
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2026-06-30 19:38:23 -0300

    Update ProvinciaService.java

--- Inmobiliaria/src/main/java/com/desi/inmobiliaria/service/ProvinciaService.java
+++ Inmobiliaria/src/main/java/com/desi/inmobiliaria/service/ProvinciaService.java
@@ -4,11 +4,16 @@ import com.desi.inmobiliaria.entity.Provincia;
 import java.util.List;
 
 public interface ProvinciaService {
+
+   // Devuelve todas las provincias
    List<Provincia> listarTodas();
 
+   // Busca una provincia por su id
    Provincia buscarPorId(Long id);
 
+   // Guarda una provincia nueva o editada
    Provincia guardar(Provincia provincia);
 
+   // Elimina una provincia
    void eliminar(Long id);
 }
